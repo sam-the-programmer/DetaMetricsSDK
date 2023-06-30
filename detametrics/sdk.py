9a213ea8ea58e8d4315b2c9d9ddd729dd5a40aac
sam-the-programmer <85027459+sam-the-programmer@users.noreply.github.com>
2023-06-30 15:18:48 +0100

    push

--- detametrics/sdk.py
+++ detametrics/sdk.py
@@ -1,8 +1,11 @@
 import httpx
 
 class DetaMetrics:
-    def __init__(self, urlId: str) -> None:
-        self.url = urlId + ".deta.app"
+    def __init__(self, urlId: str, apiKey: str) -> None:
+        self.url = "https://" + urlId + ".deta.app"
+        self.__apiKey = apiKey
+        self.__headers = {"User-Agent": "DetaMetrics", "X-Space-App-Key": self.__apiKey}
+        print(f"Using DetaMetrics at {self.url}")
 
     def __repr__(self) -> str:
         return f"<DetaMetrics url={self.url}>"
@@ -11,7 +14,7 @@ class DetaMetrics:
         return f"DetaMetrics(url={self.url})"
 
     def set(self, graph: str, metric: str, value: float) -> None:
-        httpx.get(f"https://{self.url}/set/{graph}/{metric}/{value}")
+        httpx.get(f"{self.url}/set/{graph}/{metric}/{value}", headers=self.__headers)
 
-    def clear(self, graph: str) -> None:
-        httpx.get(f"https://{self.url}/clear")
+    def clear(self) -> None:
+        httpx.get(f"{self.url}/clear", headers=self.__headers)
